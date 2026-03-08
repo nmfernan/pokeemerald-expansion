5188c1a1959726fd7bae1b2b08c418798398db3d
nicksnax <nicksnax>
2026-03-08 10:47:07 +0900

    Update to species gen tool to use openpyxl

--- pokeemerald-tools/create-new-gen-files.py
+++ pokeemerald-tools/create-new-gen-files.py
@@ -20,8 +20,31 @@ const struct SpeciesInfo gSpeciesInfo""" + GenName + """[] =
 #endif"""
 
 with open('test.h', WriteOrAdd) as file:
-    file.write(Header +  "\n")
-    if 
-    file.write(
+    #Print high level information about datafile being accessed
+    if Debug:
+        print(f"First row for species {PkmnDataFile.min_row}")
+        print(f"Last row for species {PkmnDataFile.max_row}")
+        print(f"First column of species-data {PkmnDataFile.min_column}")
+        print(f"Last column of species-data  {PkmnDataFile.max_column}")
+    
+    #Write top level information to species file
+    file.write(Header +  "\n")           
+
+    #Begin writing species information to .h file
+    if Debug == 1:
+        for species in PkmnDataFile.iter_rows(min_row=1, max_row=5, min_col=1, max_col=PkmnDataFile.max_column):
+            for data in species:
+                if data.column == PkmnDataFile.max_column and data.value == 	1:
+                    print("New Species Found")
+                print(data.value)
+                print(data.column)
+    
+    elif Debug == 0:
+        for species in PkmnDataFile.rows:
+            for data in species:
+                if data.max_column.value == 1:
+                    print("New Species Found")
+                print(data.value)
+
     if Debug:
         file.write("//end of program")
