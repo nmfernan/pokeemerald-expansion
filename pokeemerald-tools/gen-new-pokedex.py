efcf4946859ca58bcc12b05e7adf68619b83aa59
nicksnax <nicksnax>
2026-03-10 01:25:53 +0900

    finalized bugs and fixes in gen files

--- pokeemerald-tools/gen-new-pokedex.py
+++ pokeemerald-tools/gen-new-pokedex.py
@@ -14,8 +14,16 @@ PkmnData = load_workbook('pkmndata.xlsx')
 PkmnDataFile = PkmnData.active
 Region = "KANTO"
 National = "NATIONAL"
+SpeciesIndex = 1573 #based on mega_glimmora being 1572
 
 with open("new-pokedex.txt", WriteOrAdd) as file:
+    file.write("//Species File Update\n")
+    for species in PkmnDataFile.iter_rows(min_row=2, max_row=PkmnDataFile.max_row, min_col=PkmnDataFile.min_column, max_col=PkmnDataFile.max_column):
+        for data in species:
+            if PkmnDataFile.cell(row = PkmnDataFile.min_row, column = data.column).value == ".natDexNeeded" and data.value == 1:
+                file.write(f"\t#define SPECIES_ {PkmnDataFile.cell(row = data.row, column = PkmnDataFile.min_column).value} \t\t {SpeciesIndex} \n")
+                SpeciesIndex += 1
+    
     file.write("//National Dex Start\n")
     for species in PkmnDataFile.iter_rows(min_row=2, max_row=PkmnDataFile.max_row, min_col=PkmnDataFile.min_column, max_col=PkmnDataFile.max_column):
         for data in species:
