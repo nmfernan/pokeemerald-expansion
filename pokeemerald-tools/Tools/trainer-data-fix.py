25930f59668190c6e66fc8b3878b973376c73763
nicksnax <nicksnax>
2026-03-21 16:10:31 +0900

    processed all trainer and battle data for errors. base now defined

--- pokeemerald-tools/Tools/trainer-data-fix.py
+++ pokeemerald-tools/Tools/trainer-data-fix.py
@@ -1,5 +1,6 @@
 #Tool for creating new gen.h files for emerald expansion
 #Takes information from excel sheet and prepares .h file for custom pokemon
+import time
 import openpyxl as pyxl
 
 from openpyxl.utils import get_column_letter
@@ -7,16 +8,15 @@ from openpyxl.workbook import Workbook
 from openpyxl import load_workbook
 
 #Globals for making header, opening data, debug prints, etc
-Debug = 1
+Debug = 0
 WriteOrAdd = 'w'
 
 FileName = "trainer-data-fix.xlsx"
 GenName = "PkmnEvolved"
 
 PkmnData = load_workbook('trainer-data.xlsx')
-SheetName = "low-level-data-raw"
-#SheetName = "next-data"
-
+#SheetName = "low-level-data-raw"
+SheetName = "low-level-data-working"
 
 if SheetName in PkmnData.sheetnames:
     PkmnDataFile = PkmnData[SheetName]
@@ -27,6 +27,8 @@ else:
 #whatFix = "encounter-data"
 whatFix = "encounter-data"
 
+start = time.time()
+
 if Debug:
     print(f"First row for species {PkmnDataFile.min_row}")
     print(f"Last row for species {PkmnDataFile.max_row}")
@@ -35,49 +37,32 @@ if Debug:
     print(f"First column of tutor-data #{PkmnDataFile.min_column}, Letter:{get_column_letter(PkmnDataFile.min_column)}")
     print(f"Last column of tutor-data  #{PkmnDataFile.max_column}, Letter:{get_column_letter(PkmnDataFile.max_column)}")    
 
-if whatFix == "encounter-data":
-    for row in PkmnDataFile.iter_rows(min_row=PkmnDataFile.min_row, max_row=PkmnDataFile.max_row, min_col=PkmnDataFile.min_column, max_col=PkmnDataFile.max_column):
-    #for row in PkmnDataFile.iter_rows(min_row=PkmnDataFile.min_row, max_row=5, min_col=PkmnDataFile.min_column, max_col=PkmnDataFile.max_column):
-        data = str(row[0].value)
-#        if "@{" in data and "@}" in data:
-        if "@{" in data:
-            row[0].value = data[:data.find("@}")]
-            print(data[:data.find("@}")])
-            if Debug: print(row[0].value)
-            
-            PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
-            data = data[data.find("@{"):]
-            data = data.replace("@{","")
-            PkmnDataFile.cell(row[0].row + 1, 1).value = data
+for row in PkmnDataFile.iter_rows(min_row=2, max_row=PkmnDataFile.max_row, min_col=PkmnDataFile.min_column, max_col=1):
+#for row in PkmnDataFile.iter_rows(min_row=2, max_row=10, min_col=PkmnDataFile.min_column, max_col=1):
+    data = str(row[0].value)
+    if "@{" in data:
+        row[0].value = data[:data.find("@{")]
+        if Debug: print(row[0].value)
+        
+        PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
+        data = data[data.find("@{"):]
+        data = data.replace("@{","")
+        PkmnDataFile.cell(row[0].row + 1, 1).value = data
 
-        elif "@{" in data:
-            row[0].value = data[:data.find("@{")]
-            if Debug: print(data[:data.find("@{")])
-            
-            PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
-            data = data[data.find("@{"):]
-            data = data.replace("@{","")
-            PkmnDataFile.cell(row[0].row + 1, 1).value = data
+#     elif "@{" in data:
+#         row[0].value = data[:data.find("@{")]
+#         if Debug: print(data[:data.find("@{")])
+#         
+#         PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
+#         data = data[data.find("@{"):]
+#         data = data.replace("@{","")
+#         PkmnDataFile.cell(row[0].row + 1, 1).value = data
 
-        elif "@}" in data:
-            row[0].value = data.replace("@}","")
-            if Debug: print(row[0].value)
-            PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
-            PkmnDataFile.cell(row[0].row + 1, 1).value = "NEW TRAINER"
-            
-#            if "%" in data:            
-#                PkmnDataFile.cell(row[0].row + 1 , 1).value = data[data.find("%")-2:]
-
-if whatFix == "next-data":
-    for row in PkmnDataFile.iter_rows(min_row=PkmnDataFile.min_row, max_row=PkmnDataFile.max_row, min_col=PkmnDataFile.min_column, max_col=PkmnDataFile.max_column):
-        data = str(row[0].value)
-        if "{" in data:
-            row[0].value = data[:data.find("{")]
-            if Debug: print(row[0].value)
-            PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
-            PkmnDataFile.cell(row[0].row + 1, 1).value = data.replace("{","")
-            PkmnDataFile.cell(row[0].row + 1 , 1).value = data[data.find("%")-2:]
-
+    elif "@}" in data:
+        row[0].value = data.replace("@}","")
+        if Debug: print(row[0].value)
+        PkmnDataFile.insert_rows(row[0].row + 1, amount = 1)
+        PkmnDataFile.cell(row[0].row + 1, 1).value = "NEW_TRAINER"
 
 PkmnData.save(FileName)
 PkmnData.close()
